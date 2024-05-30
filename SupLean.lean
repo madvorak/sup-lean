b90b41f3fa590258627ba6bc765c307f8a239e6b
madvorak <martin.dvorak@matfyz.cz>
2024-05-30 14:35:05 +0200

    add flt-regular; build fails

--- SupLean.lean
+++ SupLean.lean
@@ -1,4 +1,5 @@
 import Mathlib
 import PrimeNumberTheoremAnd
+import FltRegular
 
 example : Irrational (Real.sqrt 2) := irrational_sqrt_two
--- lakefile.lean
+++ lakefile.lean
@@ -8,6 +8,9 @@ package «supLean» {
 require PrimeNumberTheoremAnd from git
   "https://github.com/AlexKontorovich/PrimeNumberTheoremAnd"
 
+require «flt-regular» from git
+  "https://github.com/leanprover-community/flt-regular"
+
 @[default_target]
 lean_lib «SupLean» {
   -- add any library configuration options here
